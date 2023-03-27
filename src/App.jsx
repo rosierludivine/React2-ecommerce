d017065f1ba3f925cc7bd7abee2a93eb6b50a076
rosierludivine <ludivine.rosier38@gmail.com>
2023-03-27 21:53:39 +0200

    tentative d'afficher une image

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    Achat()
+  },[]
+  )
+  const Achat = async (collectionName, achatData) => {
+    try {
+      const achatID = await addDoc(collection(database, collectionName), achatData);
+      console.log(achatID.id);
+    } catch (errorEvent ) {
+      console.error( errorEvent);
+    }
+  }
 
+  const [jordanCounter, setjordanCounter] = useState(0)
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <div className=" ">
+        <div className="p-10">
+          <div className="">
+            <div className=" bg-[url('./assets/Blanc&Rouge.png')] "></div>
+            <h1 className=' text-3xl text-center'>My Shoes  </h1>
+          </div>
+        </div>
+
+    {/* div ou vont se trouver mais jordan 1  */}
+    <div className=" w-full overflow-auto flex flex-row flex-nowrap gap-12 ">
+      <div className="flex flex-col items-center ">
+        <h3 className=" ">Article de Jordan 1</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+
+    {/* Affiche de l'artice jordan 1  */}
+    {/* Création des trois colonnes avec columns  */}
+    <div className="columns-3">
+          <div className="">
+            <div className="columns-3">
+              <div className=" bg-[url('./assets/jordan.jpg')]"></div>
+              <h3 className="font-title text-xl ">Jordan 1</h3>
+              <div className='flex flex-row justify-center columns-3'>
+                {/* Création d'un bouton pour ajouter un article et npour supprimer un article  */}
+                <button className="flex justify-center items-center px-1  m-5 border-black  bg-gray-200 border-solid hover:border-dotted" onClick={() => setjordanCounter((jordanCounter) => jordanCounter + 1)}>+ Ajouter</button>
+                <button className="flex justify-center items-center px-1  m-5 border-black  bg-gray-200 border-solid hover:border-dotted" onClick={() => setjordanCounter((jordanCounter) => jordanCounter - 1)}>- Supprimer</button>
+              </div>
+              {/* bouton pour envoyer au panier */}
+              <button onClick={() => Achat( 'achats', {produit: 'jordan1', quantité: {jordanCounter}})} type="button" className="text-m  font-paragraph border-2 border-white border-solid px-4  ">Acheter {jordanCounter}  paire </button>
+            </div>
+          </div>
+    </div>
+
+    
+    </div>
+    </div>
+
+     
   )
 }
 
